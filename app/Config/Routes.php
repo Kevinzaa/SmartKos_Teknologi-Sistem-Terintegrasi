84c9637093674a45a4c4216285329a778e01a76a
kevinza <fkevinza@gmail.com>
2024-12-29 21:16:43 +0700

    commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->post('report/create', 'ReportController::create');
+$routes->put('report/updateStatus/(:num)', 'ReportController::updateStatus/$1');
+$routes->get('report', 'ReportController::index');
+$routes->get('report/status/(:any)', 'ReportController::getByStatus/$1');
+$routes->get('report/stats', 'ReportController::stats');
+$routes->delete('report/delete/(:num)', 'ReportController::deleteReport/$1');
+$routes->post('auth/register', 'AuthController::register');
+$routes->post('auth/login', 'AuthController::login');
